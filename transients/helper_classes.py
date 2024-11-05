bda348945b4d5818f8d0417db55fa8fe58d13008
Martin Jelínek <mates14@gmail.com>
2024-11-05 16:45:52 +0100

    tracking fwhm in transients.py

---
+++ transients/helper_classes.py
@@ -0,0 +1,12 @@
+class Candidate:
+    def __init__(self, x, y, ra, dec, time, exp, airmass, mag, dmag, fwhm):
+        self.x = x
+        self.y = y
+        self.ra = ra
+        self.dec = dec
+        self.time = time
+        self.exp = exp
+        self.airmass = airmass
+        self.mag = mag
+        self.dmag = dmag
+        self.fwhm = fwhm
